72bf85b7f7fc48cf2e583384a4928f449a2c68d6
Cezary Witkowski <czarecoo@gmail.com>
2018-09-22 17:09:52 +0200

    super small changes

--- server.js
+++ server.js
@@ -1,19 +1,23 @@
-var app = require('express')();
-var server = require('http').createServer(app);
-var io = require('socket.io').listen(server);
+var app = require("express")();
+var server = require("http").createServer(app);
+var io = require("socket.io").listen(server);
 var port = 8080
 
-io.on('connection', function(socket){
-	console.log("Connected");
-	socket.on('change', function () {
-		console.log("Change");
-	});
-	socket.on('disconnect', function () {
-		console.log("Disconnected");
-	});
+server.listen(port, function () {
+	console.log("Listening on: " + port);
+	console.log("________________________");
 });
 
-server.listen(port, function() {
-	console.log('Listening on: '+ port);
-	console.log('________________________');
-});
+io.on("connection", function (socket) {
+	console.log("Connected ", socket.id);
+	socket.on("changeRequest", changeRequestHandler);
+	socket.on("disconnect", disconnectHandler);
+}
+);
+
+function changeRequestHandler() {
+	console.log("Change");
+}
+function disconnectHandler() {
+	console.log("Disconnected");
+}
