8df0a4bd1f240040ebea381ceb00db693305ef5b
Cezary Witkowski <czarecoo@gmail.com>
2018-09-23 19:05:31 +0200

    Few changes we get status 200 now

--- server.js
+++ server.js
@@ -40,77 +40,39 @@ function joinSession(userName, serverId) {
 
 }
 
-function updateJiraIssue(jiraUrl, jiraLogin, jiraPassword, issueIdOrKey, message){
-	var url = jiraUrl + "/rest/api/2/issue/" + issueIdOrKey + "/transitions";
-	
-	request({
-		url: url,
-		method: "POST",
-		json: true,
-		body: message,
-		auth: {user: jiraLogin, pass: jiraPassword}
-	}, function (error, response, body) {
-		if (!error && response.statusCode === 204) {
-			console.log("OK");
-		} else if (error) {
-			console.log(error);
-		} else {
-			console.log(response.statusCode);
-		}
-	});
-}
-
-function createJiraIssue(jiraUrl, jiraLogin, jiraPassword, issue){
-	var url = jiraUrl + "/rest/api/2/issue/";
-
-	request({
-		url: url,
-		method: "POST",
-		json: true,
-		body: issue,
-		auth: {user: jiraLogin, pass: jiraPassword}
-	}, function(error, response, body){
-		if(!error && response.statusCode === 204){
-			console.log("OK");
-		} else if (error) {
-			console.log(error);
-		} else {
-			console.log(response.statusCode);
-		}
-	});
-}
-
 function disconnect() {
 	console.log("Disconnected");
 }
+
+function updateJiraIssue(jiraUrl, jiraLogin, jiraPassword, issueIdOrKey, update) {
+	var url = jiraUrl + "/rest/api/3/issue/" + issueIdOrKey + "/editmeta";
+
+	request({
+		url: url,
+		method: "GET",
+		json: true,
+		body: update,
+		auth: { user: jiraLogin, pass: jiraPassword }
+	}, function (error, response, body) {
+		console.log('error:', error); // Print the error if one occurred
+		console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+		console.log('body:', body); // Print the HTML for the Google homepage.
+	}
+	);
+}
+
+
 
 //testing
 var jiraLogin = "adam96stan@gmail.com";
 var jiraPassword = "Cedynia97@";
 var jiraUrl = "https://adamjestem.atlassian.net";
 var jiraProject = "ToTylkoDoPobrania";
-var jiraProjectKey = "TOT"
-//createSessionWithJira("userName", jiraLogin, jiraPassword, jiraUrl, jiraProject);
-var message = {
-	"update": {
-		"comment": [{"add": {"body": "Test comment."}}]
-	},
-	"transition": {"id": "21"}
-};
-var issue = {
-    "fields": {
-		"project": {
-			"key": jiraProjectKey
-		},
-		"summary": "your summary",
-		"description": "your description",
-		"issuetype": {
-			"name": "your issuetype"
-		},
-		"priority": {
-			"name": "Minor"
-		}
+var jiraProjectKey = "TOT";
+
+var update = {
+	"fields": {
+		"customfield_10020": 3,
 	}
-}
-//updateJiraIssue(jiraUrl, jiraLogin, jiraPassword, "TOT-1", message)
-createJiraIssue(jiraUrl, jiraLogin, jiraPassword, issue);
+};
+updateJiraIssue(jiraUrl, jiraLogin, jiraPassword, "10004", update);
