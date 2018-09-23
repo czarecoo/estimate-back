daed2539ea7fb8610025c7c1d231d7776bc98b70
AdamStan <adam15stan@gmail.com>
2018-09-23 19:44:36 +0200

    zmienilem SessionManager'a, nie bedzie static, sorki

--- SessionManager.js
+++ SessionManager.js
@@ -1,13 +1,40 @@
+const simpleid = require("simple-id");
+
 class SessionManager {
 	constructor() {
-		this.sessions = [];
+		this.sessions = new Map();
+		this.setOfSessionIds = new Set();
 	}
-	static createSession(creator) {
-
+	createSession(creator) {
+		var id = this.generateSessionId();
+		this.sessions.set(id, {
+			sessionId: id,
+			creator: creator,
+			users: new Set(),
+			stories: []
+		})
 	}
-	static createSessionWithJira(creator, issues) {
-
+	createSessionWithJira(creator, issues, jiraUrl, jiraProject) {
+		var id = this.generateSessionId();
+		this.sessions.set(id, {
+			sessionId: id,
+			creator: creator,
+			users: [],
+			stories: issues,
+			jiraUrl: jiraUrl,
+			jiraProject: jiraProject
+		});
+	}
+	generateSessionId(){
+		while(true){
+			var id = simpleid(8, '123456789abcdefghijklmnoprstuwxyz');
+			if( !this.setOfSessionIds.has(id) ){ 
+				this.setOfSessionIds.add(id); 
+				return id; 
+			} 
+		}
 	}
 }
 
+// session = {sessionId, users, stories, jiraUrl, jiraProject}
 module.exports = SessionManager;
--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const app = require('express')();
 const server = require('http').createServer(app);
 const io = require('socket.io').listen(server);
 const port = 8080;
-const SessionManager = require('./SessionManager.js');
+const SessionManagerAdamCosZmienil = require('./SessionManager.js');
 const UserManager = require('./UserManager.js');
 const JiraManager = require('./JiraManager.js');
+
+var SessionManager = new SessionManagerAdamCosZmienil();
 
 server.listen(port, function () {
 	console.log('Listening on: ' + port);
@@ -17,6 +19,20 @@ io.on('connection', function (socket) {
 	socket.on('createSessionWithJiraRequest', createSessionWithJira);
 	socket.on('joinSessionRequest', joinSession);
 	socket.on('disconnect', disconnect);
+
+	socket.on("closeSession", closeSession());
+	socket.on("vote", vote(story, voteVal));
+	socket.on("coffee", coffee());
+	socket.on("kickUser", kickUser(user)); // user={userName, userId, userSocket}
+	socket.on("passCreator", passCreator(user));
+	socket.on("startStory", startStory(story));
+	socket.on("finishStory", finishStory(story));
+	socket.on("markAsFuture", markAsFuture(story));												
+	socket.on("revote", revoteStory(story));
+
+	socket.on("addEmptyStory", addEmptyStory(story));
+	socket.on("saveStory", savesStory(story));
+	socket.on("finalVote", finalVote(story));
 });
 
 function createSession(userName) {
@@ -30,12 +46,69 @@ function createSessionWithJira(userName, jiraLogin, jiraPassword, jiraUrl, jiraP
 	var issuesPromise = JiraManager.getIssues(jiraLogin, jiraPassword, jiraUrl, jiraProject);
 	var creator = UserManager.createCreator(userName);
 	issuesPromise.then(function (issues) {
-		console.log(issues);
-		SessionManager.createSessionWithJira(creator, issues);
+		try{
+			console.log(issues);
+			SessionManager.createSessionWithJira(creator, issues, jiraUrl, jiraProject);
+			console.log(SessionManager.sessions);
+		} catch(ex){
+			console.log("Exception: " + ex.constructor.name + ", message: " + ex.message);
+		}
 	})
 }
 
 function joinSession(userName, serverId) {
+	var user = UserManager.createUser(userName);
+	if(SessionManager.sessions.has(serverId)){
+		SessionManager.sessions.get(serverId).users.push(user);
+		//io.emit("grejt dżojn my frind", greatJoin);
+	}
+	else {
+		//io.emit("you are so stupid... wrong server id, bitch", wrongJoin);
+	}
+}
+
+function closeSession(sessionId){
+	SessionManager.sessions.delete(sessionId);
+	SessionManager.setOfSessionIds.delete(sessionId);
+}
+
+function vote(story, voteVal){
+
+}
+
+function coffee(sessionId){
+
+}
+
+function kickUser(user){
+	
+}
+
+function passCreator(story){
+
+}
+
+function finishStory(story){
+
+}
+
+function markAsFuture(story){
+
+}
+
+function revoteStory(story){
+
+}
+
+function addEmptyStory(story){
+
+}
+
+function savesStory(){
+
+}
+
+function finalVote(){
 
 }
 
